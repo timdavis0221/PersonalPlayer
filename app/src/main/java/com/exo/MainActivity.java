cf9d0a315467b0ff557ee53dbabed9554d837b39
Tim Davis <timdavis10310@gmail.com>
2019-02-21 13:05:31 +0800

    first commit

---
+++ app/src/main/java/com/exo/MainActivity.java
@@ -0,0 +1,159 @@
+package com.exo;
+
+import android.media.session.PlaybackState;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final  String TAG = getClass().getSimpleName();
+    private SimpleExoPlayer player;
+    private PlayerView playerView;
+    private String status;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initPLayer();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void initPLayer() {
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector();
+        RenderersFactory renderersFactory = new DefaultRenderersFactory(this);
+        player = ExoPlayerFactory.newSimpleInstance(this, renderersFactory, trackSelector);
+
+        playerView = findViewById(R.id.local_player_view);
+        playerView.setPlayer(player);
+        playerView.requestFocus();
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
+                this, Util.getUserAgent(this, "PersonalPlayer"));
+
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse("https://bestvpn.org/html5demos/assets/dizzy.mp4"));
+
+//        ConcatenatingMediaSource concatenatingMediaSource =
+//                new ConcatenatingMediaSource(videoSource);
+
+        player.prepare(videoSource);
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                Log.d(TAG, "onPlayerStateChanged: ");
+                switch (playbackState) {
+
+//                    case Player.STATE_BUFFERING:
+//                        status = PlaybackStatus.LOADING;
+//                        break;
+//                    case Player.STATE_ENDED:
+//                        status = PlaybackStatus.STOPPED;
+//                        break;
+//                    case Player.STATE_IDLE:
+//                        status = PlaybackStatus.IDLE;
+//                        break;
+//                    case Player.STATE_READY:
+//                        status = playWhenReady ? PlaybackStatus.PLAYING : PlaybackStatus.PAUSED;
+//                        break;
+//                    default:
+//                        status = PlaybackStatus.IDLE;
+//                        break;
+                }
+            }
+
+            @Override
+            public void onSeekProcessed() {
+                Log.d(TAG, "onSeekProcessed: ");
+            }
+        });
+        // player.seekTo(0, 1);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(player != null){
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
